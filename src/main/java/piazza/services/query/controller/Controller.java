7b24e1a3ca9a6fa3306156e05f42f59cb90f8e82
Roger Shultz <rshultz@radiantblue.com>
2016-02-29 15:12:33 -0500

    Response is now dataID, added LICENSE

--- src/main/java/piazza/services/query/controller/Controller.java
+++ src/main/java/piazza/services/query/controller/Controller.java
@@ -1,8 +1,24 @@
+/**
+ * Copyright 2016, RadiantBlue Technologies, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
 package piazza.services.query.controller;
 
-//import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
@@ -14,10 +30,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-//import com.fasterxml.jackson.core.JsonParseException;
-//import com.fasterxml.jackson.databind.JsonMappingException;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-
 @RestController
 public class Controller {
 
@@ -28,18 +40,18 @@ public class Controller {
 	@RequestMapping("/")
 	@ResponseBody
 	String home() {
-		return "Hello Piazza Search Query!";
+		return "Hello World!";
 	}
 
-	@RequestMapping(value = API_ROOT + "/esDSL", method = RequestMethod.POST, consumes = "application/json")
-	public List<String> getMetadata(@RequestBody(required = true) String esDSL)/* throws JsonParseException, JsonMappingException, IOException */{
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value = API_ROOT + "/data", method = RequestMethod.GET, consumes = "application/json")
+	public List<String> getMetadata(@RequestBody(required = true) String esDSL) {
 		SearchResponse response = client.prepareSearch("pzmetadata").setTypes("DataResource").setSource(esDSL).get();
 		SearchHit[] hits = response.getHits().getHits();
 		List<String> resultsList = new ArrayList<String>();
-//		ObjectMapper mapper = new ObjectMapper();
 		for (SearchHit hit : hits) {
-//			resultsList.add(mapper.readValue(hit.sourceAsString(), Metadata.class));
-			resultsList.add(hit.sourceAsString());
+			Map<String, Object> json = hit.getSource();
+			resultsList.add((String) ((HashMap<Object, Object>) json.get("dataResource")).get("dataId"));
 		}
 		return resultsList;
 	}
